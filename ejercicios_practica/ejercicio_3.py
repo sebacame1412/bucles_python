36ff28cd0222f98278834a6652e332e6f51f94a8
sebacame1412 <105613853+sebacame1412@users.noreply.github.com>
2022-06-03 19:21:11 -0300

    Ejercicio N°3 finalizado
    Completado

--- ejercicios_practica/ejercicio_3.py
+++ ejercicios_practica/ejercicio_3.py
@@ -15,4 +15,8 @@
 numeros = [1, 5, -1, 6, 10, 2, -5]
 suma = 0   # Variable ya inicializada, la suma arranca en cero
 
+for numero in numeros:
+    suma = suma + numero
+    
 print("terminamos!, el resultado final almacenado en suma debe ser 18")
+print("Suma es igual a", suma)
