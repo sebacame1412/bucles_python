b9ef2c7b048a7605aea53a0bf3426b9a6bcd4001
sebacame1412 <105613853+sebacame1412@users.noreply.github.com>
2022-06-03 19:24:02 -0300

    Ejercicio N°2 finalizado
    Completado

--- ejercicios_profunidzacion/profundizacion_2.py
+++ ejercicios_profunidzacion/profundizacion_2.py
@@ -33,5 +33,53 @@ Se debe debe imprimir un cartel de error si el operador ingresado no es
 alguno de lo soportados o no es la palabra "FIN".
 '''
 
+from __future__ import division
+
+
 print("Mi Calculadora (^_^)")
 # Empezar aquí la resolución del ejercicio
+
+numero_1 = 8
+numero_2 = 2
+
+print("Para utilizar la calculadora debe ingresar el simbolo de la ecuación que desea realizar")
+print("Para finalizar debe escribir la palabra FIN")
+simbolo = str(input("Ingrese el simbolo de la ecuacion que desea realizar\n"))
+
+while simbolo == "fin":
+    break
+
+if simbolo == "+":
+     suma = numero_1 + numero_2
+     print("El resultado de la suma entre los numeros es:", suma)
+     
+
+if simbolo == "-":
+         resta = numero_1 - numero_2
+         print("El resultado de la resta entre los numeros es:", resta)
+         
+if simbolo == "*":
+        multiplicacion = numero_1 * numero_2
+        print("El resultado de la multiplicación entre los numeros es:", multiplicacion)
+              
+if simbolo == "/":
+        division = numero_1 / numero_2
+        print("El resultado de la división entre los numeros es:", division)
+        
+if simbolo == "**":
+        exponente = numero_1 ** numero_2
+        print("El exponente entre los numeros es:", exponente)
+        
+#  while simbolo != "+" or not "-" or not "*" or not "/" or not "**":
+#    print("Debe ingresar un caracter valido, vuelva a intentarlo")
+#    continue
+#  Quise intentar esto para que el programa se repita hasta que se
+#  escriba la palabra fin pero no me salio, me rindo.
+
+
+
+
+
+
+               
+
